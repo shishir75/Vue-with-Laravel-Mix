45b57de6109caaeb133ea65e67bd222fef3660dc
shishir75 <shishir.srdr16@gmail.com>
2020-02-21 23:31:50 +0600

    Initial Commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="notification">
+        <h2>{{ message }}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: 'Hello World'
+            }
+        }
+    }
+</script>
